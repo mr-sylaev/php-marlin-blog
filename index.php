50256e2609141156ca034580c7d45cf752ee8518
mr-sylaev <mr.sylaev@mail.ru>
2019-07-18 08:07:41 +0300

    First commit All is ready

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+    $data = [
+        [
+            'id' => 1,
+            'name' => 'rasyl',
+            'email' => 'mr.sylaev@mail.ru'
+        ],
+        [
+            'id' => 2,
+            'name' => 'maxim',
+            'email' => 'mr.maxim@mail.ru'
+        ]
+    ];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Homepage</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>User management</h1>
+				<a href="create.html" class="btn btn-success">Add User</a>
+				
+				<table class="table">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Username</th>
+							<th>Email</th>
+							<th>Actions</th>
+						</tr>
+					</thead>
+
+					<tbody>
+                        <?php foreach($data as $user): ?>
+                        <tr>
+                            <td><?=$user['id'];?></td>
+                            <td><?=$user['name'];?></td>
+                            <td><?=$user['email'];?></td>
+                            <td>
+                                <a href="edit.html" class="btn btn-warning">Edit</a>
+                                <a href="#" onclick="return confirm('are you sure?')" class="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+                        <?php endforeach; ?>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
