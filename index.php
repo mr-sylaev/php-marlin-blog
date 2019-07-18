b75482a1f0c98e7ae2dbffcb0c58b859602092fc
mr-sylaev <mr.sylaev@mail.ru>
2019-07-18 11:05:02 +0300

    last saved

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
                         <?php foreach ($data as $user) : ?>
                         <tr>
                             <td><?=$user['id'];?></td>
-                            <td><?=$user['username'];?></td>
+                            <td><?=$user['name'];?></td>
                             <td><?=$user['email'];?></td>
                             <td>
                                 <a href="edit.html" class="btn btn-warning">Edit</a>
